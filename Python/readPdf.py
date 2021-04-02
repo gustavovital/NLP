c1eff560ab4cba4cf108c0483ad24c3b1af4f4b1
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-04-02 06:53:48 +0100

    new func

--- Python/readPdf.py
+++ Python/readPdf.py
@@ -8,6 +8,7 @@
 # Modules to import:
 import PyPDF2
 import glob
+import re
 
 # Declarar a pasta com os arquivos Pdfs a serem lidos:
 DataPath = "Data/"  # se estiver na pasta raiz, deixar só duas aspas: ""
@@ -22,18 +23,39 @@ for file in glob.glob("{}*.pdf".format(DataPath)):
 
 
 def readPDF(file):
+
     with open(file, "rb") as pdfFile:
+
         readPdf = PyPDF2.PdfFileReader(pdfFile)
         numberOfPages = readPdf.getNumPages()
         text = ""
+
         for pageNumber in range(numberOfPages):
             page = readPdf.getPage(pageNumber)
             pageContent = page.extractText()
             text += pageContent
+
     text = text.replace("\n", "")
+
     return text
 
 # Lendo todos os pdfs de uma pasta, por exemplo:
 
 for file in pdfFiles:
     print(readPDF(file))
+
+# Definindo uma função para criação de corpus
+
+
+def createCorpus(Files):  # files tem que estar em formato de lista
+
+    names = []
+    try:
+        index =
+        for file in Files:
+
+            print(file)
+    except TypeError:
+        print("O argumento da função tem que ser uma lista.")
+
+createCorpus(pdfFiles)
